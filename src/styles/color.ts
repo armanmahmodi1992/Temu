ddaf35ec016989f1a232c0d0c0fc31eb2753949d
Arman Mahmodi <arman_m121@yahoo.com>
2023-06-12 19:52:22 +0330

    implement color file

---
+++ src/styles/color.ts
@@ -0,0 +1,100 @@
+export const PRIMARY = '#e8eaf6';
+export const PRIMARY_LIGHT = '#ffffff';
+export const PRIMARY_DARK = '#b6b8c3';
+export const SECONDARY = '#f96509';
+export const SECONDARY_LIGHT = '#ff9642';
+export const SECONDARY_DARK = '#be3300';
+export const TEXT_ON_PRIMARY = '##000000';
+export const TEXT_ON_SECONDARY = '##000000';
+
+export const GARY_1 = '#333333';
+export const GARY_2 = '#4F4F4F';
+export const GARY_3 = '#828282';
+export const GARY_4 = '#AEAFB4';
+export const GARY_5 = '#DEE0E5';
+export const GARY_6 = '#eeeeee';
+
+export const INFO = '#3583EB';
+export const SUCCESS = '#1CA24A';
+export const WARNING = '#FFBD0A';
+export const ERROR = '#BF1A1A';
+export const LINK = '#0288d1';
+const colors = {
+    transparent: 'transparent',
+    keyboardBackgroundColor: '#D6D8DE',
+    gunMetalTransparent: '#2A3443'.concat('00'),
+    modalDimmer: '#2A3443CC', // 80%
+    errorBackground: '#E8505B17',
+    error: '#FE5656',
+    white: '#FFFFFF',
+    black: '#000000',
+    mountainMeadow: '#37C38B',
+    neonCarrot: '#ffa144',
+    gunMetal: '#2A3443',
+    slateGray: '#687D8C',
+    darkerSlateGray: '#647B8D',
+    whiteSmoke: '#F4F5F6',
+    ashGrey: '#B2BCC4', // prev: '#B3BDC5'
+    lavenderGray: '#C1C9D0',
+    silverChalice: '#A7ABB2',
+    englishGreen: '#154F38',
+    sacramentoStateGreen: '#033622',
+    honeyDev: '#EAF9F3',
+    mintCream: '#EAFFF0',
+    mintcream: '#E7F7F1',
+    nickel: '#707070',
+    isabelLine: '#EBEAEA',
+    deepSaffron: '#FFA144',
+    darkOrange: '#F98E25',
+    steelBlue: '#3782C3',
+    blackCoral: '#566273',
+    amber: '#FFC118',
+    cultured: '#EDEFF1',
+    lightCultured: '#F9FAFA',
+    sizzlingRed: '#E8505B',
+    redSalsa: '#EB5757',
+    platinum: '#E7EAED',
+    maximumBlueGreen: '#37BAC3',
+    magentaCrayola: '#EF5DA8',
+    dilutedMint: '#D7F3E8',
+    gainsboro: '#D8DFE4',
+    mountainMaize: '#EFD07A',
+    eerieBlack: '#1E1E1E',
+    silverSand: '#BBBEC3',
+    charcoal: '#2F3948',
+    ghostwhite: '#F6F8FC',
+    lightGray: '#CED6DD',
+    skeletonHighlightColor: '#E3E4E5',
+    chatBackground: '#C2DED3',
+    prompt: '#8C9CAA',
+    subtitle: '#B6B9BB',
+    alertsYellow: '#F0A500',
+    secondary100: '#F85980',
+    primary100: '#7A5DDD',
+    title: '#1B1A17',
+    black_1: '#202936',
+    close: '#5F6772',
+    divider: '#DDD',
+    ShipCove: '#768CAD',
+    BlackRock: '#222330',
+    Whisper: '#F6F3F4',
+    WaikawaGrey: '#5A7296',
+    Mischka: '#ADAEB2',
+    Gainsboro: '#DAD8D8',
+    Solitude: '#FCFDFD',
+    ShipCove_1: '#8597B2',
+    WhiteLilac: '#E3E3E4',
+    Solitude_1: '#F3F5F6',
+    WaikawaGrey_1: '#627A9D',
+    BermudaGrey: '#657C9F',
+    StormGrey: '#717279',
+    Heather: '#B3BDC5',
+    INFO : '#3583EB',
+    SUCCESS : '#1CA24A',
+    WARNING : '#FFBD0A',
+    ERROR : '#BF1A1A',
+    LINK : '#0288d1',
+  };
+  
+  export default colors;
+  
